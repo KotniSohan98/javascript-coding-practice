b3182717ac9351329f180329d53cbfbede64ee95
KotniSohan98 <kotnisohan09@gmail.com>
2025-01-17 23:48:57 +0530

    Added arguments to the main function

--- coding/main.js
+++ coding/main.js
@@ -8,22 +8,22 @@ const ele = document.querySelector("input");
 // });
 
 //after debouncing
-const outcome = () => {
-  console.log("Data received from server");
+const outcome = (data) => {
+  console.log("Data received from server", data);
 };
 
 //Here, why we are returning function inside hofDebounce to acheive the closure for timer which persist the timer for all event function called.
 function hofDebounce(fn, timeOut) {
   let timer;
-  return () => {
+  return (...args) => {
     console.log("timer", timer);
 
     clearTimeout(timer);
     timer = setTimeout(() => {
-      fn();
+      fn(args);
     }, timeOut);
   };
 }
 
 const result = hofDebounce(outcome, 1000);
-ele.addEventListener("keyup", result);
+ele.addEventListener("keyup", (event) => result(event.target.value));
