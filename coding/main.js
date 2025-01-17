714c8a289b6001089a807400f13d5d8108226b07
KotniSohan98 <kotnisohan09@gmail.com>
2025-01-17 23:26:32 +0530

    Added debounce function

--- coding/main.js
+++ coding/main.js
@@ -1,7 +1,29 @@
 console.log("Hello World");
 
-// const ele = document.querySelector("input");
+const ele = document.querySelector("input");
 
+//before debouncing
 // ele.addEventListener("keyup", () => {
 //   console.log("Data received from server");
 // });
+
+//after debouncing
+const outcome = () => {
+  console.log("Data received from server");
+};
+
+//Here, why we are returning function inside hofDebounce to acheive the closure for timer which persist the timer for all event function called.
+function hofDebounce(fn, timeOut) {
+  let timer;
+  return () => {
+    console.log("timer", timer);
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn();
+    }, timeOut);
+  };
+}
+
+const result = hofDebounce(outcome, 1000);
+ele.addEventListener("keyup", result);
